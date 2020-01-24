db25b7fd74c4d61e35979302ca0241d26ca37222
trover <overlordik1@gmail.com>
2020-01-24 14:13:58 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Fractol.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fractol.dir/main.c.o"
+  "Fractol"
+  "Fractol.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Fractol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Fractol.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Fractol.
+# This may be replaced when dependencies are built.
